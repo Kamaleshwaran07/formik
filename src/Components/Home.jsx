a0cf3ecde4c4b96a720051462fff8ee5afe5ae00
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-05 08:25:42 +0530

    Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+// import {image} from '../assets/stacked-books.jpeg'
+import '../home.css'
+
 
 const Home = () => {
-    return (
-        <div>
-          Home  
+  return (
+    <div className='home'>
+
+      <div className='container position d-flex'>
+        <div className='content me-5'>
+          <h5 className='text-lg'>Start your reading journey tomorrow</h5>
+          <h1 className='text-6xl pacifico-regular mt-2'>Where every page is a new adventure</h1>
+          <br />
+          <p>From classics to contemporary, our bookstore offers a wide selection of books to suit every taste and interest.
+            Start exploring our shelves today and uncover your next literary gem</p>
         </div>
-    );
+
+        <img src='https://cdn.discordapp.com/attachments/1018124413176135700/1214391811565428787/iStock-1087508538.webp?ex=65f8f182&is=65e67c82&hm=5b7f7911c0c27f4c7e69fbf40d1964d4ef9ce5a8a7769463d69d50e7b32690ad&' width={'600px'} className='image rounded' alt='bookstack' />
+      </div>
+
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../navbar.css'
+
+
+
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className='container-fluid'>
-                <div className='navbar-brand'>Book World</div>
+                <div className='navbar-brand font-bold fs-5'>Book World</div>
                 {/* <div className='center'> */}
 
-                    <ul className='navbar-nav flex-row justify-content-center'>
-                        <li className='nav-item p-2'><Link to={'/'} className='nav-link'>Home</Link></li>
-                        <li className='nav-item p-2'><Link to={'/body'} className='nav-link'>Body</Link></li>
+                <ul className='navbar-nav flex-row justify-content-center'>
+                    <li className='nav-item p-2 font-bold'><Link to={'/'} className='nav-link'>Home</Link></li>
+                    <li className='nav-item p-2 font-bold'><Link to={'/body'} className='nav-link'>Body</Link></li>
 
-                    </ul>
-                        <li className='nav-item d-flex flex-row p-2'><Link to={'/create'} className='nav-link'>Create</Link></li>
+                </ul>
+                <div className='nav-item font-bold create p-2'><Link to={'/create'} className='nav-link'>Create</Link></div>
                 {/* </div> */}
 
             </div>
