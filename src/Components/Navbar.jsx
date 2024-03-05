83ccabe9dc347891f68d2b609753c71d0dd0fc74
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-05 06:54:30 +0530

    navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../navbar.css'
 const Navbar = () => {
     return (
-        <div>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/body'}>Body</Link>
-            <Link to={'/create'}>Create</Link>
-        </div>
+        <nav className='navbar'>
+            <div className='container-fluid'>
+                <div className='navbar-brand'>Book World</div>
+                {/* <div className='center'> */}
+
+                    <ul className='navbar-nav flex-row justify-content-center'>
+                        <li className='nav-item p-2'><Link to={'/'} className='nav-link'>Home</Link></li>
+                        <li className='nav-item p-2'><Link to={'/body'} className='nav-link'>Body</Link></li>
+
+                    </ul>
+                        <li className='nav-item d-flex flex-row p-2'><Link to={'/create'} className='nav-link'>Create</Link></li>
+                {/* </div> */}
+
+            </div>
+        </nav>
     );
 };
 
